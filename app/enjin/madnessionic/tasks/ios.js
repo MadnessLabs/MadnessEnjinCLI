0ee2158cd11b8d9a5d54d0a7e20fd939ea927b8e
Bobby Johnson <popcorn245@gmail.com>
2016-11-04 02:04:05 -0500

    Fix ios build command

--- app/enjin/madnessionic/tasks/ios.js
+++ app/enjin/madnessionic/tasks/ios.js
@@ -2,11 +2,14 @@ const exec = require('child_process').exec;
 
 
 module.exports = function(gulp, callback) {
-    exec('gulp build --e=' + deploy.env, function(error, stdout, stderr) {
-        console.log(stdout);
-        exec('ionic build ios', function(error, stdout, stderr) {
-            console.log(stdout);
-            callback();
-        });
-    });
+    runSequence(
+        'clean:build', 
+        'config:build', 
+        'html:template',
+        'html:build', 
+        'css:build', 
+        'js:build',
+        'ios:build',
+        callback
+    );
 };
