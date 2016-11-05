051b0e7a296716ba0c9104f443a16f9947356e69
Bobby Johnson <popcorn245@gmail.com>
2016-11-05 01:55:29 -0500

    Update gulpfile to allow for custom tasks per project

--- app/boilerplates/madnessionic/gulpfile.js
+++ app/boilerplates/madnessionic/gulpfile.js
@@ -4,6 +4,7 @@ fs           = require('fs');
 gulp         = require('gulp');
 env          = JSON.parse(fs.readFileSync('.env'));
 startEnjin   = require(env.enjinPath + '/app/enjin/' + env.type);
+taskDir      = env.taskDir ? process.cwd() + '/' + env.taskDir : process.cwd() + '/tasks';
 
  /////////////////////////////////////
 // ON LOAD
@@ -15,3 +16,10 @@ require('gulp-require-tasks')({
     path: env.enjinPath + 'app/enjin/' + env.type + '/tasks',
     gulp: gulp
 });
+
+if (fs.existsSync(taskDir)) {
+    require('gulp-require-tasks')({
+        path: taskDir,
+        gulp: gulp
+    });
+}
