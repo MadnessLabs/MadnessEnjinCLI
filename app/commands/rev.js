d4d18b9ff8ed404f301863e831e9bd58de3e2dd8
Bobby Johnson <popcorn245@gmail.com>
2016-11-14 01:58:56 -0600

    Add component, controller, modal, and popover

--- app/commands/rev.js
+++ app/commands/rev.js
@@ -16,6 +16,34 @@ module.exports = function(enjinDir) {
             port: 5227
         }, function(err, bs) {
             bs.io.sockets.on('connection', function(socket) {
+                socket.on('enjin-add-component', function(data) {
+                    console.log(`Adding ${data.name} component ...`);
+                    exec(`gulp add:component -n ${data.name} -a ${data.attrs} -r ${data.resolves}`, process.cwd(), function() {
+                        console.log(`${data.name} component added successfully! ^_^`);
+                    });
+                });
+
+                socket.on('enjin-add-controller', function(data) {
+                    console.log(`Adding ${data.name} controller ...`);
+                    exec(`gulp add:controller -n ${data.name} -d ${data.dir}`, process.cwd(), function() {
+                        console.log(`${data.name} controller added successfully! ^_^`);
+                    });
+                });
+
+                socket.on('enjin-add-modal', function(data) {
+                    console.log(`Adding ${data.name} modal ...`);
+                    exec(`gulp add:modal -n ${data.name}`, process.cwd(), function() {
+                        console.log(`${data.name} modal added successfully! ^_^`);
+                    });
+                });
+
+                socket.on('enjin-add-popover', function(data) {
+                    console.log(`Adding ${data.name} popover ...`);
+                    exec(`gulp add:popover -n ${data.name}`, process.cwd(), function() {
+                        console.log(`${data.name} popover added successfully! ^_^`);
+                    });
+                });
+
                 socket.on('enjin-add-page', function(data) {
                     console.log(`Adding ${data.name} page ...`);
                     exec(`gulp add:page -n ${data.name}`, process.cwd(), function() {
