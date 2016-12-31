36914ed959074d0dd6f5354f88da6250466e4809
Bobby Johnson <popcorn245@gmail.com>
2016-12-31 17:10:12 -0600

    Add proxy and browser setting to enjin

--- app/enjin/madnessionic/tasks/sync/start.js
+++ app/enjin/madnessionic/tasks/sync/start.js
@@ -1,17 +1,27 @@
 module.exports = function(gulp, callback) {
-    browserSync.init({
+    var bsOpts = {
         port: 3000,
         files: ['index.html', '**/*.js'],
+        reloadDebounce: 1000,
         injectChanges: true,
         logFileChanges: true,
-        logLevel: 'silent',
+        logLevel: 'info',
         logPrefix: appName,
-        notify: true,
-        reloadDelay: 0,
-        server: {
+        notify: true
+    };
+    if (configJSON.proxy) {
+        bsOpts.proxy = configJSON.proxy;
+        bsOpts.open = 'external';
+        bsOpts.host = configJSON.proxy;
+    } else {
+        bsOpts.server = {
             baseDir: appDir
-        }
-    });
+        };
+    }
+    if (configJSON.browser) {
+        bsOpts.browser = configJSON.browser;
+    }
+    browserSync.init(bsOpts);
     global.synced = true;
     callback();
 };
