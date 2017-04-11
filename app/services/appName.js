2cc657fca3a5762096b2fe6e07a1bc16d6ad00d1
Bobby Johnson <popcorn245@gmail.com>
2017-04-11 01:50:11 -0500

    Add plugin and fix app name capping

--- app/services/appName.js
+++ app/services/appName.js
@@ -2,5 +2,5 @@ const cleanString = require('./cleanString');
 
 
 module.exports = function (string) {
-    return cleanString(string.charAt(0).toUpperCase() + string.slice(1));
+    return cleanString(string);
 };
