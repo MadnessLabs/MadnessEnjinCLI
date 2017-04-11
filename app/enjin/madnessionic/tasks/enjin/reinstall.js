88a4793b428bad484a9ebbafcd48fed69ede75db
Bobby Johnson <popcorn245@gmail.com>
2017-04-11 11:58:20 -0500

    Update reinstall command to add default route

--- app/enjin/madnessionic/tasks/enjin/reinstall.js
+++ app/enjin/madnessionic/tasks/enjin/reinstall.js
@@ -1,11 +1,23 @@
 const runSequence = require('run-sequence').use(gulp);
+const addPage = require('../../services/addPage');
 
 
 module.exports = function(gulp, callback) {
+    if (!configJSON.routes.length) {
+        addPage('home', []);
+    }
+
     runSequence(
+        'clean:build',
         'js:app',
+        'config:platform',
+        'config:run',
         'config:js', 
         'config:css', 
+        'config:ionic',
+        'config:cordova', 
+        'config:node', 
+        'config:sublime',
         'typings',
         'font:copy',
         'img:icon:favorite',
