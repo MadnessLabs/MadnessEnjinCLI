223e2f31ec3c54328a8597aeeb677ec4026af33b
Bobby Johnson <popcorn245@gmail.com>
2017-04-17 17:22:59 -0500

    Remove some debugging and try to fix user detection

--- app/commands/deploy.js
+++ app/commands/deploy.js
@@ -37,10 +37,9 @@ module.exports = function(enjinDir) {
                     console.log('Enabling hooks...');
                     exec(`chmod +x post-receive`, {cwd: hooksDir}, function(error, stdout, stderr){
                         exec(`id -u ${user}`, {cwd: hooksDir}, function(error, stdout, stderr){
-                            console.log(stdout, error, stderr);
-                            if (Number.isNaN(Number(stdout))) {
+                            if (!stderr && Number.isNaN(Number(stdout))) {
                                 exec(`echo ${token} | passwd ${user} --stdin`, {cwd: hooksDir}, function(error, stdout, stderr){
-                                    console.log(stdout, error, stderr);
+                                    console.log(stdout, stderr);
                                     console.log(`User created to deploy to MadnessEnjin.net...`);
                                     grantRights(user, repoDir, () => {
                                         console.log('Successfully setup deploy on server! ^_^');
