0ad8c73dce4f0aba2e3256af3457eeb459743fe2
Bobby Johnson <popcorn245@gmail.com>
2017-04-17 22:31:24 -0500

    Fix group issue

--- app/services/grantRights.js
+++ app/services/grantRights.js
@@ -2,7 +2,7 @@ const exec    = require('child_process').exec;
 
 
 module.exports = function(user, directory, callback) {
-    exec(`chown ${user}:${user} ${directory}`, {cwd: directory }, function(error, stdout, stderr){
+    exec(`chown ${user}:enjineers ${directory}`, {cwd: directory }, function(error, stdout, stderr){
         if (callback && typeof callback === 'function') {
             callback(stdout, stderr);
         }
