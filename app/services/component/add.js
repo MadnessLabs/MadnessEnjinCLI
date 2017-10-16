637642eee68d1d9c249c1fc4afb12bbc368c4813
Bobby Johnson <bobby@madnesslabs.net>
2017-10-16 16:26:38 -0500

    Fix #3 - Fix class names and assume namespacing

--- app/services/component/add.js
+++ app/services/component/add.js
@@ -1,6 +1,7 @@
 const exec = require('child_process').exec;
 const fs = require('fs-extra');
 const path = require("path");
+const argv    = require('yargs').argv;
 
 const getStencilConfig = require('../getStencilConfig');
 const editStencilConfig = require('../editStencilConfig');
@@ -10,29 +11,35 @@ const renderComponent = require('./render');
 
 
 module.exports = function(name, props) {
-    if (name.indexOf('-') <= 0) {
-        console.log('Name must contain a "-" to be a valid custom element!');
-        return false;
-    }
-
-    name = name.toLowerCase();
-
-    if (props) {
-        if (props.indexOf(',') > 0) {
-            props = props.split(',');
-        } else {
-            props = [props];
-        }
-    }
-
-    var data = {
-        name,
-        className: capFirstLetter(camelize(name.replace('-', ' '))),
-        props,
-        content: `Your new ${name} component`
-    };
-
     getStencilConfig((stencilConfig, stencilPath) => {
+        var namespace = argv.namespace ? argv.namespace : argv.n ? argv.n : stencilConfig.namespace;
+
+        if (namespace && namespace !== true) {
+            name = namespace + '-' + name;
+        }
+
+        if (name.indexOf('-') <= 0) {
+            console.log('Name must contain a "-" to be a valid custom element!');
+            return false;
+        }
+    
+        name = name.toLowerCase();
+    
+        if (props) {
+            if (props.indexOf(',') > 0) {
+                props = props.split(',');
+            } else {
+                props = [props];
+            }
+        }
+    
+        var data = {
+            name,
+            className: capFirstLetter(camelize(name.replace(new RegExp('-', 'g'), ' '))),
+            props,
+            content: `Your new ${name} component`
+        };
+
         renderComponent(data, stencilConfig, () => {
             stencilConfig.bundles[0].components.push(name);
             editStencilConfig(stencilPath, stencilConfig, () => {
