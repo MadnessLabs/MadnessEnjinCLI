6c5c866fba92d4a54dbedd2e0a5baa37c894f8cd
Bobby Johnson <bobby@madnesslabs.net>
2018-03-19 17:16:13 -0500

    Fix #5 - Only replace bundles in stencil config

--- app/services/editStencilConfig.js
+++ app/services/editStencilConfig.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra');
 
 
-module.exports = function(filePath, configJSON, callback) {
+module.exports = function (filePath, configJSON, callback) {
     if (filePath.indexOf('enjin') >= 0) {
         var enjinJSON = require(filePath);
         enjinJSON.stenciljs = configJSON;
@@ -15,14 +15,14 @@ module.exports = function(filePath, configJSON, callback) {
             }
         });
     } else {
-        fs.readFile(filePath, 'utf8', function(error, data) {
+        fs.readFile(filePath, 'utf8', function (error, data) {
             if (error) {
                 console.log('Couldn\'t read stencil.config.js!');
                 return false;
             }
-    
-            var newStencilJS = data.replace(/exports.config ?= ?[^;]*/, 'exports.config = ' + JSON.stringify(configJSON, null, 2));
-            fs.writeFile(filePath, newStencilJS, function(error) {
+
+            var newStencilJS = data.replace(/bundles ?: ?[^,]*/, 'bundles: ' + JSON.stringify(configJSON.bundles, null, 2));
+            fs.writeFile(filePath, newStencilJS, function (error) {
                 if (error) {
                     console.log('Couldn\'t write stencil.config.js!');
                     return false;
