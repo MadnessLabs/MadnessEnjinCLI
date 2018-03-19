7f795823f68422919196f62577ee5ef81862114f
Bobby Johnson <bobby@madnesslabs.net>
2018-03-19 16:59:36 -0500

    Version Bump Update Version in Readme Move props to being an optional flag

--- app/commands/add.js
+++ app/commands/add.js
@@ -1,15 +1,15 @@
 const addComponent = require('../services/component/add');
 
 
-module.exports = function(enjinDir) {
+module.exports = function (enjinDir) {
     var type = process.argv[3];
     var name = process.argv[4];
 
     console.log(`Creating ${name} ${type}...`);
 
-    switch(type) {
+    switch (type) {
         case 'component':
-            addComponent(name, process.argv[5]);
+            addComponent(name);
             break;
         default:
             console.log(`Cannot create ${type}!`);
